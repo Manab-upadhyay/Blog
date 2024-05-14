72fb802ccad53a55bac707cb06f72912037cb5a8
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2024-05-14 19:40:33 +0530

    new

--- app/db/page.js
+++ app/db/page.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(' mongodb://127.0.0.1:27017/myblogs', {
+    await mongoose.connect('mongodb://localhost:27017/myblogs', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
