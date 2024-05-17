af6224d0d90ec7cb65aba38b8d513372e5b3e508
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2024-05-18 00:37:27 +0530

    db1

--- app/db/page.js
+++ app/db/page.js
@@ -1,21 +1,17 @@
-import {MongoClient } from "mongodb"
-
-const uri = "mongodb://manab123:qwerty123@ac-neq9zbn-shard-00-00.hphb4ra.mongodb.net:27017,ac-neq9zbn-shard-00-01.hphb4ra.mongodb.net:27017,ac-neq9zbn-shard-00-02.hphb4ra.mongodb.net:27017/?ssl=true&replicaSet=atlas-eihh8b-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("Connected to MongoDB");
-
-    const db = client.db("test");
-    const collection = db.collection("devices");
-
-    // Return the client and collection to allow further operations
-    return { client, collection };
-  } catch (err) {
-    console.error("Failed to connect to MongoDB", err);
-    throw err;
+    await mongoose.connect('mongodb://manab123:qwerty123@ac-neq9zbn-shard-00-00.hphb4ra.mongodb.net:27017,ac-neq9zbn-shard-00-01.hphb4ra.mongodb.net:27017,ac-neq9zbn-shard-00-02.hphb4ra.mongodb.net:27017/?ssl=true&replicaSet=atlas-eihh8b-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
   }
 };
 
-export default connectDB
+
+export default connectDB;
