401cba80804744d5894cfa29c15637320b4eccf9
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2024-05-17 22:30:17 +0530

    chng

--- app/compose/page.js
+++ app/compose/page.js
@@ -58,7 +58,9 @@ useEffect(()=>{
       try {
         const response = await fetch("/posts", {
           method: "POST",
-         
+          headers: {
+            "Content-Type": "application/json"
+          },
           body: JSON.stringify(data),
         });
 
