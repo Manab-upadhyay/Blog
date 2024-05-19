657ed4a5755ce656ea54a28d1157a6d8aa4bde0c
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2024-05-19 20:05:37 +0530

    manab

--- app/posts/api/route.ts
+++ app/posts/api/route.ts
@@ -8,7 +8,7 @@ export async function GET() {
     console.log('Database connection established');
     let posts = await Posts.find();
     console.log('Posts fetched:', posts);
-    return Response.json({message:'fetched again',posts})
+    return Response.json({posts})
   } catch (error) {
     console.error('Error fetching posts:', error);
     return NextResponse.json({ error: 'Failed to fetch posts' }, { status: 500 });
