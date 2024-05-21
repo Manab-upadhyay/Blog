ae42a1f2dfae127f9af7bff8daf8b51e89ccbb57
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2024-05-21 20:24:20 +0530

    rech

--- app/posts/api/route.ts
+++ app/posts/api/route.ts
@@ -1,6 +1,4 @@
-import Posts from "../../models/post";
-
-
+import Posts from '../../models/post';
 import connectDB from '../../db/page';
 import { NextResponse } from 'next/server';
 
@@ -9,26 +7,8 @@ export async function GET() {
     await connectDB();
     console.log('Database connection established');
     let posts = await Posts.find();
-    console.log('Posts fetched:', posts);
 
-    // Create a response object with the posts and a message
-    const responseObject = {
-      message: 'Refetched data successfully',
-      posts,
-    };
-
-    // Create response with no-cache headers
-    const response = new NextResponse(JSON.stringify(responseObject), {
-      headers: {
-        'Content-Type': 'application/json',
-        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-        'Pragma': 'no-cache',
-        'Expires': '0',
-        'Surrogate-Control': 'no-store',
-      },
-    });
-
-    return response;
+    return NextResponse.json(posts);
   } catch (error) {
     console.error('Error fetching posts:', error);
     return NextResponse.json({ error: 'Failed to fetch posts' }, { status: 500 });
