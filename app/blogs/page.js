05e914a9bc18e873d9be3ec28c38dda9e0a19e98
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2024-07-31 10:50:50 +0530

    spinner set

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,32 +1,26 @@
 "use client";
 import React, { useEffect, useState } from "react";
-const calsans= require('cal-sans')
-
-import Image from "next/image"
+import calsans from "cal-sans";
+import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 import { TracingBeam } from "../componets/blogui";
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import { deepOrange, deepPurple } from '@mui/material/colors';
-const jwt= require('jsonwebtoken')
-
-
-
-
-
+import Avatar from "@mui/material/Avatar";
+import { deepOrange } from "@mui/material/colors";
+import jwt from "jsonwebtoken";
+import Loading from "../myblogs/loading";
 
 export default function TracingBeamDemo() {
-  const [post, setPost] = useState();
+  const [post, setPost] = useState([]);
   const [expandedIndex, setExpandedIndex] = useState(-1);
-  const [searchTerm, setSearchTerm] = useState(""); // Initialize with -1 to indicate no post is expanded
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('/posts', {
-          cache: 'no-store' 
-          
-        })
+          cache: 'no-store',
+        });
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
@@ -35,14 +29,18 @@ export default function TracingBeamDemo() {
         setPost(data);
       } catch (error) {
         console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
+
   const filteredPosts = post?.filter((item) =>
     item?.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
   const renderContent = (content) => {
     const paragraphs = content.split('\n');
     return paragraphs.map((paragraph, index) => (
@@ -53,86 +51,97 @@ export default function TracingBeamDemo() {
   };
 
   const handleRead = (index) => {
-    setExpandedIndex(index); // Update the expanded index to the clicked post index
+    setExpandedIndex(index);
   };
 
   return (
-    <TracingBeam className="px-6 my-28 w-full ">
-      <div className="max-w-2xl mx-auto antialiased pt-4 relative">
-      <input
-          type="text"
-          placeholder="Search by title"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full p-2 rounded border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
-        />
-        {!searchTerm&&post?.map((item, index) => (
-          <div key={index} className="mb-10 my-5">
-            <Avatar sx={{ bgcolor: deepOrange[500] }}>{item?.name.charAt(0)}</Avatar>
-            <h1>Post By: {item?.name}</h1>
-            <p className={twMerge(calsans.className, "text-xl mb-4")}>
-              {item?.title}
-            </p>
-            <div className="text-sm prose prose-sm dark:prose-invert">
-              <img
-                src={item?.image}
-                alt="blog thumbnail"
-                height="1000"
-                width="1000"
-                className="rounded-lg mb-10 object-cover"
-              />
-              {expandedIndex === index ? (
-                <div key={index}>{renderContent(item.content)}</div>
-              ) : (
-                <div key={index}>{renderContent(item.content.slice(0, 300))}</div>
-              )}
-              {expandedIndex !== index && (
-                <p
-                  key={`read-more-${index}`}
-                  className="text-blue-700 cursor-pointer"
-                  onClick={() => handleRead(index)}
-                >
-                  Read More
-                </p>
-              )}
-            </div>
-          </div>
-        ))}
-        {searchTerm&&filteredPosts?.map((item, index) => (
-          <div key={index} className="mb-10">
-            <Avatar sx={{ bgcolor: deepOrange[500] }}>{item.name.charAt(0)}</Avatar>
-            <h1>Post By: {item.name}</h1>
-            <p className={twMerge("text-xl mb-4")}>
-              {item.title}
-            </p>
-            <div className="text-sm prose prose-sm dark:prose-invert">
-              <img
-                src={item?.image}
-                alt="blog thumbnail"
-                height="1000"
-                width="1000"
-                className="rounded-lg mb-10 object-cover"
-              />
-              {expandedIndex === index ? (
-                <div key={index}>{renderContent(item.content)}</div>
-              ) : (
-                <div key={index}>{renderContent(item.content.slice(0, 1000))}</div>
-              )}
-              {expandedIndex !== index && (
-                <p
-                  key={`read-more-${index}`}
-                  className="text-blue-700 cursor-pointer"
-                  onClick={() => handleRead(index)}
-                >
-                  Read More
-                </p>
-              )}
-            </div>
-          </div>
-        ))}
-        
-      </div>
+    <TracingBeam className="px-6 my-28 w-full">
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <Loading />
+        </div>
+      ) : (
+        <div className="max-w-2xl mx-auto antialiased pt-4 relative">
+          <input
+            type="text"
+            placeholder="Search by title"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full p-2 rounded border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+          />
+          {!searchTerm
+            ? post?.map((item, index) => (
+                <div key={index} className="mb-10 my-5">
+                  <Avatar sx={{ bgcolor: deepOrange[500] }}>
+                    {item?.name.charAt(0)}
+                  </Avatar>
+                  <h1>Post By: {item?.name}</h1>
+                  <p className={twMerge(calsans.className, "text-xl mb-4")}>
+                    {item?.title}
+                  </p>
+                  <div className="text-sm prose prose-sm dark:prose-invert">
+                    <img
+                      src={item?.image}
+                      alt="blog thumbnail"
+                      height="1000"
+                      width="1000"
+                      className="rounded-lg mb-10 object-cover"
+                    />
+                    {expandedIndex === index ? (
+                      <div key={index}>{renderContent(item.content)}</div>
+                    ) : (
+                      <div key={index}>
+                        {renderContent(item.content.slice(0, 300))}
+                      </div>
+                    )}
+                    {expandedIndex !== index && (
+                      <p
+                        key={`read-more-${index}`}
+                        className="text-blue-700 cursor-pointer"
+                        onClick={() => handleRead(index)}
+                      >
+                        Read More
+                      </p>
+                    )}
+                  </div>
+                </div>
+              ))
+            : filteredPosts?.map((item, index) => (
+                <div key={index} className="mb-10">
+                  <Avatar sx={{ bgcolor: deepOrange[500] }}>
+                    {item.name.charAt(0)}
+                  </Avatar>
+                  <h1>Post By: {item.name}</h1>
+                  <p className={twMerge("text-xl mb-4")}>{item.title}</p>
+                  <div className="text-sm prose prose-sm dark:prose-invert">
+                    <img
+                      src={item?.image}
+                      alt="blog thumbnail"
+                      height="1000"
+                      width="1000"
+                      className="rounded-lg mb-10 object-cover"
+                    />
+                    {expandedIndex === index ? (
+                      <div key={index}>{renderContent(item.content)}</div>
+                    ) : (
+                      <div key={index}>
+                        {renderContent(item.content.slice(0, 1000))}
+                      </div>
+                    )}
+                    {expandedIndex !== index && (
+                      <p
+                        key={`read-more-${index}`}
+                        className="text-blue-700 cursor-pointer"
+                        onClick={() => handleRead(index)}
+                      >
+                        Read More
+                      </p>
+                    )}
+                  </div>
+                </div>
+              ))}
+        </div>
+      )}
     </TracingBeam>
   );
 }
-
--- app/myblogs/page.js
+++ app/myblogs/page.js
@@ -1,56 +1,51 @@
 "use client";
 import React, { useEffect, useState } from "react";
-const calsans= require('cal-sans')
-
-import Image from "next/image"
+const calsans = require('cal-sans');
+import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 import { TracingBeam } from "../componets/blogui";
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Avatar from '@mui/material/Avatar';
-
-
-
-const jwt= require('jsonwebtoken')
-
-
-
-
+import Loading from "./loading";
+const jwt = require('jsonwebtoken');
 
 export default function TracingBeamDemo() {
- 
-  const [post, setPost] = useState();
-  const [expandedIndex, setExpandedIndex] = useState(-1); // Initialize with -1 to indicate no post is expanded
-const[sing, setsing]= useState(false)
+  const [post, setPost] = useState([]);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+  const [signedIn, setSignedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchData = async () => {
-        const token = localStorage.getItem('token')
-        if(token){
-            setsing(true)
-            const gettoken= jwt.decode(token)
-            const id= gettoken.email
-            console.log("email>>", id)
-        
-    
+      const token = localStorage.getItem('token');
+      if (token) {
+        setSignedIn(true);
+        const decodedToken = jwt.decode(token);
+        const email = decodedToken.email;
+        console.log("email>>", email);
 
-      try {
-        const response = await fetch('/posts');
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
+        try {
+          const response = await fetch('/posts');
+          if (!response.ok) {
+            throw new Error('Failed to fetch data');
+          }
+          const data = await response.json();
+          const filteredData = data.filter(item => item.userid === email);
+          console.log('Response from API:', filteredData);
+          setPost(filteredData);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        } finally {
+          setLoading(false);
         }
-        const data = await response.json();
-        const filterdata= data.filter((item )=> item.userid==id)
-        console.log('Response from API:', filterdata);
-        setPost(filterdata);
-      } catch (error) {
-        console.error('Error fetching data:', error);
+      } else {
+        setLoading(false);
       }
-    }
     };
 
     fetchData();
   }, []);
-
 
   const renderContent = (content) => {
     const paragraphs = content.split('\n');
@@ -60,74 +55,81 @@ const[sing, setsing]= useState(false)
       </p>
     ));
   };
-  async function handdledelete(postid){
 
-    const posts= {postid}
-        try {
-            const data = await fetch("/delete", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(posts),
-              });
-            
-            if (data.ok) {
-            window.location.reload()
-            }
-            
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
+  const handleDelete = async (postid) => {
+    try {
+      const response = await fetch("/delete", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ postid }),
+      });
+
+      if (response.ok) {
+        setPost((prevPosts) => prevPosts.filter(post => post.postid !== postid));
+      }
+    } catch (error) {
+      console.error('Error deleting post:', error);
     }
+  };
+
   const handleRead = (index) => {
-    setExpandedIndex(index); // Update the expanded index to the clicked post index
+    setExpandedIndex(index);
   };
 
   return (
     <TracingBeam className="px-6 my-28 md:w-full w-72">
-      {sing &&<div className="max-w-2xl mx-auto antialiased pt-4 relative">
-        {post?.map((item, index) => (
-          <div key={index} className="mb-10">
-            
-            <p className={twMerge(calsans.className, "text-xl mb-4")}>
-              {item.title}
-            </p>
-            <div className="text-sm prose prose-sm dark:prose-invert">
-              <img
-                src={item?.image}
-                alt="blog thumbnail"
-                height="1000"
-                width="1000"
-                className="rounded-lg mb-10 object-cover"
-              />
-              {expandedIndex === index ? (
-                <div key={index}>{renderContent(item.content)}</div>
-              ) : (
-                <div key={index}>{renderContent(item.content.slice(0, 1000))}</div>
-              )}
-              {expandedIndex !== index && (
-                <p
-                  key={`read-more-${index}`}
-                  className="text-blue-700 cursor-pointer"
-                  onClick={() => handleRead(index)}
-                >
-                  Read More
-                </p>
-
-              )}
-<Button variant="outlined" startIcon={<DeleteIcon />}  onClick={() => handdledelete(item.postid)}>
-        Delete
-      </Button>
-            </div>
-          </div>
-        ))}
-      </div>}
-      {!sing&&<div className="font-blac text-center ">sing up/ login to see your blogs</div>}
-      {post?.length===0
-        &&<p className="text-center">No blogs yet</p>
-      }
+      {loading ? (
+        <div className="flex justify-center items-center min-h-screen">
+          <Loading />
+        </div>
+      ) : signedIn ? (
+        <div className="max-w-2xl mx-auto antialiased pt-4 relative">
+          {post?.length > 0 ? (
+            post.map((item, index) => (
+              <div key={index} className="mb-10">
+                <Avatar>{item.title.charAt(0)}</Avatar>
+                <p className={twMerge(calsans.className, "text-xl mb-4")}>{item.title}</p>
+                <div className="text-sm prose prose-sm dark:prose-invert">
+                  <img
+                    src={item?.image}
+                    alt="blog thumbnail"
+                    height="1000"
+                    width="1000"
+                    className="rounded-lg mb-10 object-cover"
+                  />
+                  {expandedIndex === index ? (
+                    <div key={index}>{renderContent(item.content)}</div>
+                  ) : (
+                    <div key={index}>{renderContent(item.content.slice(0, 300))}</div>
+                  )}
+                  {expandedIndex !== index && (
+                    <p
+                      key={`read-more-${index}`}
+                      className="text-blue-700 cursor-pointer"
+                      onClick={() => handleRead(index)}
+                    >
+                      Read More
+                    </p>
+                  )}
+                  <Button
+                    variant="outlined"
+                    startIcon={<DeleteIcon />}
+                    onClick={() => handleDelete(item.postid)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p className="text-center">No blogs yet</p>
+          )}
+        </div>
+      ) : (
+        <div className="font-black text-center">Sign up/ login to see your blogs</div>
+      )}
     </TracingBeam>
   );
 }
-
